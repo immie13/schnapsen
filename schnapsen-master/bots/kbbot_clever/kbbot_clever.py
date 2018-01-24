613030afcd5ff5d49e86383b295fcb9c89ad3e98
louisepos <35340560+louisepos@users.noreply.github.com>
2018-01-24 09:30:15 +0100

    Question 9 van worksheet 2. 24/1/2017 voor practicum.

--- schnapsen-master/bots/kbbot_clever/kbbot_clever.py
+++ schnapsen-master/bots/kbbot_clever/kbbot_clever.py
@@ -26,14 +26,19 @@ class Bot:
 
         for move in moves:
 
-            if not self.kb_consistent(state, move):
+            if not self.kb_consistent_trumpace(state, move):
                 # Plays the first move that makes the kb inconsistent. We do not take
                 # into account that there might be other valid moves according to the strategy.
                 # Uncomment the next line if you want to see that something happens.
                 # print "Strategy Applied"
 
-                print "   Is the trump marriage applied? Card played:"
-                print str(util.get_card_name(move[0]))
+                print "Card played:"
+                print util.get_card_name(move[0])
+                return move
+
+            if not self.kb_consistent_jack(state, move):
+                print "Card played:"
+                print util.get_card_name(move[0])
                 return move
 
         # If no move that is entailed by the kb is found, play random move
@@ -41,7 +46,7 @@ class Bot:
 
     # Note: In this example, the state object is not used,
     # but you might want to do it for your own strategy.
-    def kb_consistent(self, state, move):
+    def kb_consistent_trumpwedding(self, state, move):
     # type: (State, move) -> bool
 
         # each time we check for consistency we initialise a new knowledge-base
@@ -80,13 +85,10 @@ class Bot:
         # PlayJack heuristics that was defined in class. Initialise a different variable if 
         # you want to apply a different strategy (that you will have to define in load.py)
         variable_string = "ptw" + str(index)
-        variable_string2 = "ptw" + str(index2)
         strategy_variable = Boolean(variable_string)
-        strategy_variable2 = Boolean(variable_string2)
 
         # Add the relevant clause to the loaded knowledge base
         kb.add_clause(~strategy_variable)
-        kb.add_clause(~strategy_variable2)
 
         # If the knowledge base is not satisfiable, the strategy variable is
         # entailed (proof by refutation)
@@ -98,4 +100,75 @@ class Bot:
             print move
             print kb.satisfiable()
 
+        return kb.satisfiable()
+
+
+    def kb_consistent_trumpace(self, state, move):
+    # type: (State, move) -> bool
+
+        # each time we check for consistency we initialise a new knowledge-base
+        kb = KB()
+
+        # Add general information about the game
+
+        suit = State.get_trump_suit(state)
+        if suit == "c":
+            kb.add_clause(Boolean("ta0"))
+        elif suit == "d":
+            kb.add_clause(Boolean("ta5"))
+        elif suit == "h":
+            kb.add_clause(Boolean("ta10"))
+        elif suit == "s":
+            kb.add_clause(Boolean("ta15"))
+
+        load_clever.general_information(kb)
+
+        # Add the necessary knowledge about the strategy
+        load_clever.strategy_knowledge(kb)
+
+        # This line stores the index of the card in the deck.
+        # If this doesn't make sense, refer to _deck.py for the card index mapping
+        index = move[0]
+
+        variable_string = "pta" + str(index)
+        strategy_variable = Boolean(variable_string)
+
+        # Add the relevant clause to the loaded knowledge base
+        kb.add_clause(~strategy_variable)
+
+        # If the knowledge base is not satisfiable, the strategy variable is
+        # entailed (proof by refutation)
+
+        return kb.satisfiable()
+
+    def kb_consistent_jack(self, state, move):
+    # type: (State, move) -> bool
+
+        # each time we check for consistency we initialise a new knowledge-base
+        kb = KB()
+
+        # Add general information about the game
+        load_clever.general_information(kb)
+
+        # Add the necessary knowledge about the strategy
+        load_clever.strategy_knowledge(kb)
+
+        # This line stores the index of the card in the deck.
+        # If this doesn't make sense, refer to _deck.py for the card index mapping
+        index = move[0]
+
+        # This creates the string which is used to make the strategy_variable.
+        # Note that as far as kb.py is concerned, two objects created with the same
+        # string in the constructor are equivalent, and are seen as the same symbol.
+        # Here we use "pj" to indicate that the card with index "index" should be played with the
+        # PlayJack heuristics that was defined in class. Initialise a different variable if
+        # you want to apply a different strategy (that you will have to define in load.py)
+        variable_string = "pj" + str(index)
+        strategy_variable = Boolean(variable_string)
+
+        # Add the relevant clause to the loaded knowledge base
+        kb.add_clause(~strategy_variable)
+
+        # If the knowledge base is not satisfiable, the strategy variable is
+        # entailed (proof by refutation)
         return kb.satisfiable()
